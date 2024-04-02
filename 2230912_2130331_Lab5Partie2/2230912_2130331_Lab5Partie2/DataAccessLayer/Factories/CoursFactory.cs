fbd24a7f0b04ef9da8ffce51bc57aceb2ceffcb6
Nelson Junior Yimou Noubissi <2130331@etu.cchic.ca>
2024-04-02 14:50:24 -0400

    première factory faite

---
+++ 2230912_2130331_Lab5Partie2/2230912_2130331_Lab5Partie2/DataAccessLayer/Factories/CoursFactory.cs
@@ -0,0 +1,22 @@
+﻿using _2230912_2130331_Lab5Partie2.DataAccessLayer.Factories.Base;
+using _2230912_2130331_Lab5Partie2.Models;
+using MySql.Data.MySqlClient;
+
+namespace _2230912_2130331_Lab5Partie2.DataAccessLayer.Factories
+{
+    public class CoursFactory : FactoryBase
+    {
+        private Cours CreateFromReader(MySqlDataReader mySqlDataReader)
+        {
+            string code = mySqlDataReader["cou_sigle"].ToString();
+            string titre = mySqlDataReader["cou_titre"].ToString();
+            int duree = (int)mySqlDataReader["cou_duree"];
+
+            return new Cours(code, titre, duree);
+
+        }
+
+
+        //Faire les autres méthodes en fonction de ce qui est demandé dans l'énoncé du travail
+    }
+}
