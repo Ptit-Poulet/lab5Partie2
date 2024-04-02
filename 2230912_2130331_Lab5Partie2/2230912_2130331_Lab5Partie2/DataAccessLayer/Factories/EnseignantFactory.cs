26335a7c7bd7ee067139ebfdc088eb758bda91bb
Samara Boudreault <2230912@etu.cchic.ca>
2024-04-02 15:27:00 -0400

    Début de chaque factory fini

---
+++ 2230912_2130331_Lab5Partie2/2230912_2130331_Lab5Partie2/DataAccessLayer/Factories/EnseignantFactory.cs
@@ -0,0 +1,21 @@
+﻿using _2230912_2130331_Lab5Partie2.Models;
+using MySql.Data.MySqlClient;
+
+namespace _2230912_2130331_Lab5Partie2.DataAccessLayer.Factories
+{
+    public class EnseignantFactory
+    {
+        private Enseignant CreateFromReader(MySqlDataReader mySqlDataReader)
+        {
+            int id = (int)mySqlDataReader["en_id"];
+            string nom = mySqlDataReader["en_nom"].ToString();
+            string prenom = mySqlDataReader["en_prenom"].ToString();
+            int numeroEmploye = (int)mySqlDataReader["en_numero_employe"];
+            string dateEmbauche = mySqlDataReader["en_date_embauche"].ToString();
+            string dateRetraite = mySqlDataReader["en_date_retraite"].ToString();
+
+
+            return new Enseignant(id, nom, prenom, numeroEmploye, dateEmbauche, dateRetraite);
+        }
+    }
+}
