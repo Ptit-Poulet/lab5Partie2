4deb4674f849fef9f3eac6d4544c719fc0e932f0
Samara <142835909+Ptit-Poulet@users.noreply.github.com>
2024-04-02 13:47:44 -0400

    Début du projet
    Programme créé

---
+++ 2230912_2130331_Lab5Partie2/2230912_2130331_Lab5Partie2/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace _2230912_2130331_Lab5Partie2
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
