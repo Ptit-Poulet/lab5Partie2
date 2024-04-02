e96b743b32eabcfa997f414b3bf79f5b1f9c04d5
Samara <142835909+Ptit-Poulet@users.noreply.github.com>
2024-04-02 14:04:24 -0400

    Add files via upload

---
+++ 2230912_2130331_Lab5Partie2/2230912_2130331_Lab5Partie2/DataAccessLayer/DAL.cs
@@ -0,0 +1,6 @@
+﻿namespace _2230912_2130331_Lab5Partie2.DataAccessLayer
+{
+    public class DAL
+    {
+    }
+}
---
+++ 2230912_2130331_Lab5Partie2/2230912_2130331_Lab5Partie2/DataAccessLayer/Factories/Base/FactoryBase.cs
@@ -0,0 +1,25 @@
+﻿using _2230912_2130331_Lab5Partie2.SQL;
+
+namespace _2230912_2130331_Lab5Partie2.DataAccessLayer.Factories.Base
+{
+    public class FactoryBase
+    {
+        private string _cnnStr = string.Empty;
+
+        public string CnnStr
+        {
+            get
+            {
+                if (_cnnStr == string.Empty)
+                {
+                    var config = new ConfigurationBuilder().SetBasePath(AppDomain.CurrentDomain.BaseDirectory).AddJsonFile("appsettings.json").Build();
+                    var sectionConnectionString = config.GetSection("ConnectionString");
+                    Lab5Config connectionString = sectionConnectionString.Get<Lab5Config>();
+                    _cnnStr = connectionString.BuildConnectionString();
+                }
+
+                return _cnnStr;
+            }
+        }
+    }
+}
