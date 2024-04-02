0aa7f5523c4070a5f27d49f3afdb32a2f5457da3
Nelson Junior Yimou Noubissi <2130331@etu.cchic.ca>
2024-04-02 14:21:02 -0400

    1ere modif

---
+++ 2230912_2130331_Lab5Partie2/2230912_2130331_Lab5Partie2/Models/Cours.cs
@@ -0,0 +1,13 @@
+﻿namespace _2230912_2130331_Lab5Partie2.Models
+{
+    public class Cours
+    {
+        
+        /// <summary>
+        /// Code du cours 
+        /// </summary>
+        public string  CodeCours { get; set; }
+
+        
+    }
+}
