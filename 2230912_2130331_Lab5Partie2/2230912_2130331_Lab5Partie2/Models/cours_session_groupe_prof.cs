cd6617dc6b7d530eec2d58e16290f5381c41290e
Samara Boudreault <2230912@etu.cchic.ca>
2024-04-02 14:53:03 -0400

    models fini

--- 2230912_2130331_Lab5Partie2/2230912_2130331_Lab5Partie2/Models/cours_session_groupe_prof.cs
+++ 2230912_2130331_Lab5Partie2/2230912_2130331_Lab5Partie2/Models/cours_session_groupe_prof.cs
@@ -1,6 +1,16 @@
 ﻿namespace _2230912_2130331_Lab5Partie2.Models
 {
-    public class cours_session_groupe_prof
+    /// <summary>
+    /// Classe qui représente un cours, une session, un group et un prof
+    /// </summary>
+    public class Cours_session_groupe_prof
     {
+        public int csgp_id { get; set; }    
+        public string csgp_sigle_cours { get; set; }
+
+        public int csgp_id_session { get; set; }
+        public int csgp_groupe { get; set; }
+        public int csgp_id_prof { get; set; }
+
     }
 }
